ed677752ef9468ac7cc0b33ebcdbb2f0a0fff536
Mario Ramirez <miramirezjr@gmail.com>
2019-02-19 14:58:43 -0700

    Update groupDirectories.pp

--- groupDirectories.pp
+++ groupDirectories.pp
@@ -1,6 +1,19 @@
 file { '/home/managers':
 	ensure => "directory",
-	owner => '',
-	group => '',
+	owner => 'mscott',
+	group => 'managers',
+	mode => '2770',
+	source => ["/etc/skel"],
+}
+file { '/home/sales':
+	ensure => "directory",
+	owner => 'abernard',
+	group => 'sales',
+	mode => '2770',
+}
+file { '/home/accounting':
+	ensure => "directory",
+	owner => 'amartin',
+	group => 'accounting',
 	mode => '2770',
 }
